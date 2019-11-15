20dfdc871d1670a18e81a0eac733cbf58791a048
WoodMan <woodmanpublic@gmail.com>
2019-11-15 23:21:55 +0800

    Fix host

--- server.py
+++ server.py
@@ -38,5 +38,5 @@ def index():
     return status.getStatus()
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('', 5000), app)
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
     http_server.serve_forever()
