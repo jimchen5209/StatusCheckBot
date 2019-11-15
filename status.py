36815e71023045ccfae7aeed975a3dd7dfd8fc61
WoodManGitHub <cheeseman0930@gmail.com>
2019-11-15 18:00:23 +0800

    fix all

--- status.py
+++ status.py
@@ -13,13 +13,13 @@ def getStatus():
 
     for walk in os.walk(path):
         for file in walk[2]:
-            f = open(path + '/' + file, 'r')
-            bot_status = json.loads(f.read())
-            try:
-                p = psutil.Process(bot_status['pid'])
-                if p.cmdline()[1] == bot_status['cmdline'][0] and p.name().startswith('python'):        
-                    status[bot_status['name']] = {'online': True}
-            except psutil.NoSuchProcess:
-                status[bot_status['name']] = {'online': False}
-            f.close()
+            with open(path + '/' + file, 'r') as fs:
+                bot_status = json.loads(fs.read())
+                try:
+                    p = psutil.Process(bot_status['pid'])
+                    if p.cmdline()[1] == bot_status['cmdline'][0] and p.name().startswith('python'):        
+                        status[bot_status['name']] = {'online': True}
+                except psutil.NoSuchProcess:
+                    status[bot_status['name']] = {'online': False}
+
     return status
