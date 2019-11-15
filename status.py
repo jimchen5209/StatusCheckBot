c05a0c05a147451081409269212f6340de6df53b
WoodManGitHub <cheeseman0930@gmail.com>
2019-11-15 18:04:24 +0800

    fix tabs

--- status.py
+++ status.py
@@ -15,11 +15,11 @@ def getStatus():
         for file in walk[2]:
             with open(path + '/' + file, 'r') as fs:
                 bot_status = json.loads(fs.read())
-                try:
-                    p = psutil.Process(bot_status['pid'])
-                    if p.cmdline()[1] == bot_status['cmdline'][0] and p.name().startswith('python'):        
-                        status[bot_status['name']] = {'online': True}
-                except psutil.NoSuchProcess:
-                    status[bot_status['name']] = {'online': False}
+            try:
+                p = psutil.Process(bot_status['pid'])
+                if p.cmdline()[1] == bot_status['cmdline'][0] and p.name().startswith('python'):        
+                    status[bot_status['name']] = {'online': True}
+            except psutil.NoSuchProcess:
+                status[bot_status['name']] = {'online': False}
 
     return status
