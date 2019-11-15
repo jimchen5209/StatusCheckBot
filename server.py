5bccfccab716bd3e9ee639e1277516c28fb1bac3
WoodManGitHub <cheeseman0930@gmail.com>
2019-11-15 22:45:41 +0800

    Fix server

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from gevent.pywsgi import WSGIServer
 import status
 
 app = Flask(__name__)
 
-@app.route('/getStatus')
+@app.route('/getStatus', methods=['GET'])
 def index():
     return status.getStatus()
 
 if __name__ == '__main__':
-    app.run()
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
